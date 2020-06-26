3e762da080472894917827244451132c258d1832
Daniel Tovar <danieldaniyyelda@gmail.com>
2020-06-26 00:26:14 -0400

    adding Gatsby project

---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+export default ({ data }) => (
+  <Layout>
+      <SEO title="About"/>
+      <h2>About us</h2>
+      <h2>{data.site.siteMetadata.title}</h2>
+      <div>{data.site.siteMetadata.description}</div>
+        <Link to="/">Back to home</Link>
+  </Layout>
+)
+
+export const query = graphql `
+  query {
+    site {
+      siteMetadata {
+        author,
+        description,
+        title
+      }
+    }
+  }
+`
